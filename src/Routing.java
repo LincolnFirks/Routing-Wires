1d879f5dcee941f69e58941a56000b6c381261ed
LincolnFirks <156463638+LincolnFirks@users.noreply.github.com>
2024-11-21 18:16:34 -0500

    Update Routing.java

--- src/Routing.java
+++ src/Routing.java
@@ -11,7 +11,37 @@ public class Routing {
      */
     public static ArrayList<Wire>
     findPaths(Board board, ArrayList<Endpoints> goals) {
-        return null;  // replace this line with your code
+        ArrayList<Wire> res = new ArrayList<>();
+        for (Endpoints e : goals) {
+            Wire curr = BFS(board, e);
+            if (curr != null) {
+                board.placeWire(curr);
+                res.add(curr);
+            }
+        }
+        return res;
+    }
+
+    public static Wire BFS(Board board, Endpoints e) {
+        Queue<Wire> Q = new LinkedList<>();
+        Q.add(new Wire(e.id,new ArrayList<Coord>( List.of(e.start))));
+        HashSet<Coord> seen = new HashSet<>();
+        seen.add(e.start);
+        while (!Q.isEmpty()) {
+            Wire curr = Q.poll();
+            if (curr.end().equals(e.end)) {
+                return curr;
+            }
+            for (Coord adj : board.adj(curr.end())) {
+                if (!seen.contains(adj) && !board.isObstacle(adj) ) {
+                    seen.add(adj);
+                    Wire newWire = new Wire(e.id, curr.getPoints());
+                    newWire.add(adj);
+                    Q.add(newWire);
+                }
+            }
+        }
+        return null;
     }
 
 }
