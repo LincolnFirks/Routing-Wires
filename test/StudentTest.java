3fe79037b2229e7a2aba6e740ea0a995b7760a0a
MysticalRobot <dhruv21420@gmail.com>
2024-12-02 18:37:13 -0500

    input file used to build an example board and a test on it that was timed

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -1,16 +1,19 @@
 import org.junit.jupiter.api.Test;
-import java.time.Duration;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.util.ArrayList;
 
 public class StudentTest {
 
     @Test
-    public void testWire0() {
-        assertTimeout(Duration.ofMillis(1000), () -> {
-            Utilities.test("./test/inputs/wire0.in");
-        });
+    public void testWire0() throws Utilities.BoardConstructionException {
+        ArrayList<Endpoints> goals1 = new ArrayList<>();
+        ArrayList<Integer[]> obstacles1 = new ArrayList<>();
+        Board b1 = Utilities.buildBoard(goals1, obstacles1, "test/inputs/ex1.in");
+        long startTime = System.nanoTime();
+        Routing.findPaths(b1, goals1);
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Execution time in milliseconds: " + duration / 1_000_000);
     }
-
-    // your tests go here
 
 }
